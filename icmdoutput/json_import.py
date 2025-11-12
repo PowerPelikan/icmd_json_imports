9cb45f7ea6428c1d6a9bbeb6144838047ef80be5
PowerPelikan <jona.hart.97@proton.me>
2025-11-12 09:23:32 +0100

    Refactoring json_import classes

--- icmdoutput/json_import.py
+++ icmdoutput/json_import.py
@@ -8,147 +8,103 @@ class JsonData:
     """Class to convert json file from icmd data into human readable lists and Dataframes"""
 
     def __init__(self, path: str):
-
         if not os.path.isfile(path):
-            raise ValueError(path + " does not exist")
+            raise FileNotFoundError(f"{path} does not exist")
+        self.data = self._import_data(path)
+        self.elememts = self._extract_elements()
+        self.models = self._extract_models()
 
-        self.data = self.__import_data(path)
-        self.elememts = self.__get_elements()
-        self.models = self.__get_models()
-
-    def __import_data(self, path: str):
-        """ open json-file and return it """
-
-        with open(path, 'r', encoding="utf-8") as f:
-            try:
-                data = json.load(f)
-            except OSError:
-                print("Cannot open given path")
-
-            return data
-
-
-    def __get_models(self):
-        """Return a list of all used models"""
-        return list(self.data['models'].keys())
-
-
-    def __get_elements(self):
-        """Return a list of all used elements"""
-
-        list_of_data = []
-        for i in self.__get_models():
-            try:
-                list_of_data.append(self.data['models'][i]['coords']['component']['data'])
-            except RuntimeError:
-                print('Error in finding model elements')
-        df = pd.DataFrame(list_of_data, index=self.__get_models()).T
-
-        return df
-
-
-    def __get_datakeys_of_models(self):
-
-        """ Return calculated model data keys """
-        list_of_data = []
-        for i in self.__get_models():
-            try:
-                list_of_data.append(self.data['models'][i]['data_vars'].keys())
-            except KeyError:
-                print('Error in finding data keys')
-        df = pd.DataFrame(list_of_data, index=self.__get_models()).T
-
-        return df
-
-    def __get_data_from_key(self, datakey: str, model: str):
-        """Function return values under given datakey and model"""
-
+    def _import_data(self, path: str) -> dict:
+        """Open json-file and return the loaded data"""
         try:
-            self.is_model(model)
-            self.is_datakey(datakey, model)
-        except ValueError:
-            return False
+            with open(path, "r", encoding='utf-8') as f:
+                return json.load(f)
+        except OSError as e:
+            raise IOError(f"Cannot open or read '{path}'") from e
 
-        return self.data["models"][model]["data_vars"][datakey]
 
-    def get_model(self):
-        """Function return used models"""
+    def _extract_models(self) -> list:
+        """Return a list of all used models"""
+        return list(self.data.get("models", {}).keys())
+
+    def _extract_elements(self) -> pd.DataFrame:
+        """Return a list of all used elements"""
+        rows = []
+        for model in self._extract_models():
+            try:
+                rows.append(self.data['models'][model]['coords']['component']['data'])
+            except KeyError:
+                print(f"Faild to extract elements for model '{model}'")
+        return pd.DataFrame(rows, index=self._extract_models()).T
+
+
+    def _extract_datakeys(self) -> pd.DataFrame:
+        """ Return calculated model data keys """
+        keys = []
+        for model in self._extract_models():
+            try:
+                keys.append(self.data['models'][model]['data_vars'].keys())
+            except KeyError:
+                print(f"Missing data keys in model '{model}'")
+        return pd.DataFrame(keys, index=self._extract_models()).T
+
+    def _get_data_var(self, data_key: str, model: str):
+        self._validate_model(model)
+        self._validate_datakey(data_key, model)
+        return self.data['models'][model]['data_vars'][data_key]
+
+    def get_models(self) -> list:
+        """Function return used models list"""
         return self.models
 
-    def get_data(self):
-        """Function return data as loaded json"""
+    def get_data(self) -> dict:
+        """Function return data as dict"""
         return self.data
 
-    def get_elements(self):
+    def get_elements(self) -> pd.DataFrame:
         """Funciton return used elements in given data as list"""
         return self.elememts
 
-    def get_datakeys_of_models(self) -> list:
+    def get_datakeys_of_models(self) -> pd.DataFrame:
         """Function return all datakey in given data as list"""
-        return self.__get_datakeys_of_models()
+        return self._extract_datakeys()
 
-    def is_model(self, model: str) -> bool:
+    def _validate_model(self, model: str) -> bool:
         """Function chekcks, if given model in given data"""
-        if model in self.__get_models():
-            return True
-        raise ValueError("Model is not in given data")
+        if model not in self._extract_models():
+            raise ValueError(f"Model '{model}' is not in data")
 
-    def check_datakeys(self, datakey: str, *model ):
+    def _validate_datakey(self, data_key: str, model:str) -> bool:
         """Function checks if given datakey is in model"""
+        if data_key not in self._extract_datakeys()[model].values:
+            raise ValueError(f"Datakey '{data_key}' not found in '{model}'")
+        return True
 
-        if not model:
-            model = self.__get_models()
-
-        for m in model:
-            self.is_model(m)
-            if datakey in self.__get_datakeys_of_models()[m].values:
-                print(datakey + "is in " + m)
-            else:
-                print("Not in " + m)
-
-    def is_datakey(self, datakey: str, model: str) -> bool:
-        """Function return, if given datakey is in given model"""
-
-        if datakey in self.__get_datakeys_of_models()[model].values:
-            return True
-        raise ValueError("Datakey " + datakey + " is not in model " + model)
-
-    def get_data_from_keys(self, datakeys: list, model):
+    def get_data_from_keys(self, data_keys: list, model: str) -> pd.DataFrame:
         """Function return values of given datakeys and models in a dataframe"""
-        list_of_data = []
-        for d in datakeys:
-            list_of_data.append(self.__get_data_from_key(d, model))
-
-        df = pd.DataFrame(list_of_data, index=datakeys).T
-
-        return df
+        values = [self._get_data_var(k, model) for k in data_keys]
+        return pd.DataFrame(values, index=data_keys).T
 
 class SingleModel(JsonData):
-    ''' Get Data from one model from the json-file'''
+    '''Access data from a single model within the JSON file'''
 
-    def __init__(self, path, modelname: str):
+    def __init__(self, path: str, model_name: str):
         super().__init__(path)
+        self._validate_model(model_name)
+        self.data = self.data['models'][model_name]
+        self.model_name = model_name
 
-        if self.is_model(modelname):
-            self.data = self.data['models'][modelname]
-            self.modelname = modelname
-    
-    def __get_gradient_component(self):
+    def _get_gradient_component(self):
         return self.data['coords']['gradient_component']['data']
     
-    def get_gradient_component(self):
+    def get_gradient_component(self) -> pd.DataFrame:
         '''Get dataframe with gradient components'''
-
-        return pd.DataFrame(self.__get_gradient_component())
+        return pd.DataFrame(self._get_gradient_component())
 
     def get_parameter_values(self):
         '''Return used Parameter values if used'''
-
         return self.data['data_vars']['parameter_values']['data']
-    
+
     def get_parameter(self):
         '''Return used Parameter if used'''
-
         return self.data['coords']['parameter']['data']
-    
-
